22501c8c7ec8a39db6ef11b7f5a4445bbdc47839
susmitha <susmitha3377@gmail.com>
2021-02-13 13:17:39 +0530

    code changes for DealsPage

---
+++ src/main/java/com/pages/DealsPage.java
@@ -0,0 +1,16 @@
+package com.pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class DealsPage {
+
+	
+		private Object driver;
+
+		public DealsPage(){
+			this.driver=driver;
+		}
+
+	
+
+}
